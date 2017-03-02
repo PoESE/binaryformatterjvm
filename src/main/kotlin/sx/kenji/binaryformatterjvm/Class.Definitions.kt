abf062928e51c1e68f18a37875456513fed5923a
KenjiTakahashi <wozniakk@gmail.com>
2017-03-02 21:44:36 +0100

    Refactor Class.Definitions module

--- src/main/kotlin/sx/kenji/binaryformatterjvm/Class.Definitions.kt
+++ src/main/kotlin/sx/kenji/binaryformatterjvm/Class.Definitions.kt
@@ -3,32 +3,15 @@ package sx.kenji.binaryformatterjvm
 import net.bytebuddy.ByteBuddy
 import net.bytebuddy.description.modifier.Visibility
 
-class ClassWithId(bf: BinaryFormatter) : RecordWithObjectId {
-    private val recordTypeEnum = RecordTypeEnumeration.ClassWithId
-
-    override val objectId = bf.stream.readInt32()
-    val metadataId = bf.stream.readInt32()
-
-    private val _name: String
-    private val _memberNames: List<String>
+open class _Class {
     val _values = mutableListOf<Any>() // FIXME: Make this private
 
-    init {
-        val scwmatPromise = bf.want(this.metadataId)
-        if(!scwmatPromise.isSuccess()) {
-            throw Exception("SHOULD BE SUCCESS") // TODO: Do better exception
-        }
-        val scwmat = scwmatPromise.get() as SystemClassWithMembersAndTypes
-
-        this._name = scwmat.name
-        this._memberNames = scwmat.memberNames
-
-        // REFACTOR: This is very similar to SCWMAT.readValues
-        for(i in 0..scwmat.memberCount-1) {
-            val bte = scwmat.memberTypeInfo.binaryTypeEnums[i]
+    open protected fun readValues(bf: BinaryFormatter, src: SystemClassWithMembersAndTypes) {
+        for(i in 0..src.memberCount-1) {
+            val bte = src.memberTypeInfo.binaryTypeEnums[i]
             when(bte) {
                 BinaryTypeEnumeration.Primitive -> {
-                    val ai = scwmat.memberTypeInfo.additionalInfos[i]!!.primitive!!
+                    val ai = src.memberTypeInfo.additionalInfos[i]!!.primitive!!
                     when(ai) {
                         PrimitiveTypeEnumeration.Int32 -> {
                             this._values.add(bf.stream.readInt32())
@@ -45,14 +28,14 @@ class ClassWithId(bf: BinaryFormatter) : RecordWithObjectId {
         }
     }
 
-    override fun create(): Any {
+    protected fun create(src: SystemClassWithMembersAndTypes): Any {
         // TODO: Make this more (hum) generic
-        if("Generic.List`1" in this._name) {
+        if("Generic.List`1" in src.name) {
             return Array(this._values[1] as Int, {
                 ((this._values[0] as Record).create() as Array<Any>)[it]
             })
         }
-        if("Generic.Dictionary`2" in this._name) {
+        if("Generic.Dictionary`2" in src.name) {
             val value = (this._values[3] as Record).create() as Array<Any>
             return mutableMapOf(*value.map({
                 Pair(
@@ -62,7 +45,7 @@ class ClassWithId(bf: BinaryFormatter) : RecordWithObjectId {
             }).toTypedArray())
         }
 
-        var name = this._name
+        var name = src.name
         if("`" in name) {
             name = name.substring(0, name.indexOf('`'))
         }
@@ -72,7 +55,7 @@ class ClassWithId(bf: BinaryFormatter) : RecordWithObjectId {
             Class.forName(name)
         } catch(e: Exception) {
             var bb = ByteBuddy().subclass(Any::class.java).name(name)
-            for(member in this._memberNames) {
+            for(member in src.memberNames) {
                 // TODO: Handle this (k__BackingField) better
                 var m = member
                 if(member.startsWith("<")) {
@@ -84,7 +67,7 @@ class ClassWithId(bf: BinaryFormatter) : RecordWithObjectId {
         }
 
         val ins = cls.newInstance()
-        for((i, member) in this._memberNames.withIndex()) {
+        for((i, member) in src.memberNames.withIndex()) {
             var value = this._values[i]
             if(value is Record) {
                 value = value.create()
@@ -97,6 +80,26 @@ class ClassWithId(bf: BinaryFormatter) : RecordWithObjectId {
         }
         return DynClass(cls, ins)
     }
+}
+
+class ClassWithId(bf: BinaryFormatter): _Class(), RecordWithObjectId {
+    private val recordTypeEnum = RecordTypeEnumeration.ClassWithId
+
+    override val objectId = bf.stream.readInt32()
+    val metadataId = bf.stream.readInt32()
+
+    private val scwmat: SystemClassWithMembersAndTypes
+
+    init {
+        val scwmatPromise = bf.want(this.metadataId)
+        if(!scwmatPromise.isSuccess()) {
+            throw Exception("SHOULD BE SUCCESS") // TODO: Do better exception
+        }
+        this.scwmat = scwmatPromise.get() as SystemClassWithMembersAndTypes
+        this.readValues(bf, this.scwmat)
+    }
+
+    override fun create(): Any = this.create(this.scwmat)
 
     override fun write(os: BinaryWriterOutputStream) {
         os.writeByte(this.recordTypeEnum.value)
@@ -115,8 +118,8 @@ class ClassWithId(bf: BinaryFormatter) : RecordWithObjectId {
     }
 }
 
-open class SystemClassWithMembersAndTypes(bf: BinaryFormatter) : RecordWithObjectId {
-    private val recordTypeEnum = RecordTypeEnumeration.SystemClassWithMembersAndTypes
+open class SystemClassWithMembersAndTypes(bf: BinaryFormatter): _Class(), RecordWithObjectId {
+    open protected val recordTypeEnum = RecordTypeEnumeration.SystemClassWithMembersAndTypes
 
     /* ClassInfo structure start */
     override val objectId = bf.stream.readInt32()
@@ -126,90 +129,15 @@ open class SystemClassWithMembersAndTypes(bf: BinaryFormatter) : RecordWithObjec
     /* ClassInfo structure end */
     val memberTypeInfo: MemberTypeInfo
 
-    val _values = mutableListOf<Any>() // FIXME: Make this private
-
     init {
         for(i in 0..this.memberCount-1) {
             this.memberNames.add(bf.stream.readString())
         }
         this.memberTypeInfo = MemberTypeInfo(bf.stream, this.memberCount)
-        this.readValues(bf)
+        this.readValues(bf, this)
     }
 
-    open protected fun readValues(bf: BinaryFormatter) {
-        for(i in 0..this.memberCount-1) {
-            val bte = this.memberTypeInfo.binaryTypeEnums[i]
-            when(bte) {
-                BinaryTypeEnumeration.Primitive -> {
-                    val ai = this.memberTypeInfo.additionalInfos[i]!!.primitive!!
-                    when(ai) {
-                        PrimitiveTypeEnumeration.Int32 -> {
-                            this._values.add(bf.stream.readInt32())
-                        }
-                        PrimitiveTypeEnumeration.Boolean -> {
-                            this._values.add(bf.stream.readBoolean())
-                        }
-                    }
-                }
-                else -> {
-                    this._values.add(bf.readRecord())
-                }
-            }
-        }
-    }
-
-    override fun create(): Any {
-        // TODO: Make this more (hum) generic
-        if("Generic.List`1" in this.name) {
-            return Array(this._values[1] as Int, {
-                ((this._values[0] as Record).create() as Array<Any>)[it]
-            })
-        }
-        if("Generic.Dictionary`2" in this.name) {
-            val value = (this._values[3] as Record).create() as Array<Any>
-            return mutableMapOf(*value.map({
-                Pair(
-                    (it as DynClass).cls.getField("key").get(it.instance),
-                    it.cls.getField("value").get(it.instance)
-                )
-            }).toTypedArray())
-        }
-
-        var name = this.name
-        if("`" in name) {
-            name = name.substring(0, name.indexOf('`'))
-        }
-        name = name.replace('+', '.')
-
-        val cls = try {
-            Class.forName(name)
-        } catch(e: Exception) {
-            var bb = ByteBuddy().subclass(Any::class.java).name(name)
-            for(member in this.memberNames) {
-                // TODO: Handle this (k__BackingField) better
-                var m = member
-                if(member.startsWith("<")) {
-                    m = member.substring(1, member.indexOf('>'))
-                }
-                bb = bb.defineField(m, Any::class.java, Visibility.PUBLIC)
-            }
-            bb.make().load(ClassLoader.getSystemClassLoader()).loaded
-        }
-
-        val ins = cls.newInstance()
-        for((i, member) in this.memberNames.withIndex()) {
-            var value = this._values[i]
-            if(value is Record) {
-                value = value.create()
-            }
-            var m = member
-            if(member.startsWith("<")) {
-                m = member.substring(1, member.indexOf('>'))
-            }
-            cls.getField(m).set(ins, value)
-        }
-        return DynClass(cls, ins)
-    }
+    override fun create(): Any = this.create(this)
 
     override fun write(os: BinaryWriterOutputStream) {
         os.writeByte(this.recordTypeEnum.value)
@@ -232,12 +160,12 @@ open class SystemClassWithMembersAndTypes(bf: BinaryFormatter) : RecordWithObjec
 }
 
 class ClassWithMembersAndTypes(bf: BinaryFormatter) : SystemClassWithMembersAndTypes(bf) {
-    private val recordTypeEnum = RecordTypeEnumeration.ClassWithMembersAndTypes
+    override val recordTypeEnum = RecordTypeEnumeration.ClassWithMembersAndTypes
 
     var libraryId: Int = 0
 
-    override fun readValues(bf: BinaryFormatter) {
+    override fun readValues(bf: BinaryFormatter, src: SystemClassWithMembersAndTypes) {
         this.libraryId = bf.stream.readInt32()
-        super.readValues(bf)
+        super.readValues(bf, src)
     }
 }
